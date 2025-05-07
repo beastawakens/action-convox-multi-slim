cb8ee83dfd27b3c9462ee9f9419e7a0d8b4ff7c5
Ed Fricker <edafricker@gmail.com>
2025-05-07 11:43:04 +0100

    Release v1.0.31

--- entrypoint-rack-param.sh
+++ entrypoint-rack-param.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 set -x
 
-echo "DEBUG: All INPUT_ environment variables:"
-env | grep INPUT_
-
 export CONVOX_RACK=$INPUT_RACK
-
-echo "PARAM_NAME: $INPUT_PARAMNAME"
-echo "PARAM_VALUE: $INPUT_PARAMVALUE"
 
 echo "Running rack params set on $INPUT_RACK for param $INPUT_PARAMNAME"
 convox rack params set $INPUT_PARAMNAME=$INPUT_PARAMVALUE
