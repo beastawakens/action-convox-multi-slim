4dec6f90e9abeced664f577d89963d98d0f79fa4
Ed Fricker <edafricker@gmail.com>
2025-07-22 10:57:55 +0100

    Release v1.0.32

--- entrypoint-run.sh
+++ entrypoint-run.sh
@@ -10,8 +10,8 @@ export CONVOX_RACK=$INPUT_RACK
 if [ -n "$RELEASE" ]
 then
   echo "Running command on $INPUT_SERVICE - $INPUT_APP for the release $RELEASE"
-  convox run $INPUT_SERVICE "$INPUT_COMMAND" --release $RELEASE --app $INPUT_APP --rack $INPUT_RACK 2>&1 | tee /dev/stdout
+  convox run $INPUT_SERVICE "$INPUT_COMMAND" --release $RELEASE --app $INPUT_APP --rack $INPUT_RACK
 else
   echo "Running command on $INPUT_SERVICE - $INPUT_APP for the latest release"
-  convox run $INPUT_SERVICE "$INPUT_COMMAND" --app $INPUT_APP --rack $INPUT_RACK 2>&1 | tee /dev/stdout
+  convox run $INPUT_SERVICE "$INPUT_COMMAND" --app $INPUT_APP --rack $INPUT_RACK
 fi
