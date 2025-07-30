67eef44d7bdff9c0349d8bba0b22f8b628abafba
Tom Cummings <tom@smileidentity.com>
2025-07-30 12:09:44 -0400

    Register the new command with the .yml and entrypoint

--- entrypoint-get-rack-param.sh
+++ entrypoint-get-rack-param.sh
@@ -3,8 +3,8 @@ set -x
 
 export CONVOX_RACK=$INPUT_RACK
 
-echo "Retrieving $RACK_PARAM for $CONVOX_RACK"
-output=$(convox rack params | awk -v param="$RACK_PARAM" '$1 == param {print $2}')
+echo "Retrieving $INPUT_PARAMNAME for $CONVOX_RACK"
+output=$(convox rack params | awk -v param="$INPUT_PARAMNAME" '$1 == param {print $2}')
 
 echo "PARAM_VALUE=$output" >> $GITHUB_OUTPUT
 echo "PARAM_VALUE=$output" >> $GITHUB_ENV
