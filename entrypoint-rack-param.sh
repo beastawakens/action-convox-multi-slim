17caaa2d272cb5b0cbab3e3d6ec84721d75adb42
Tom Cummings <tom@smileidentity.com>
2025-07-31 09:00:18 -0400

    Output the rack param value

--- entrypoint-rack-param.sh
+++ entrypoint-rack-param.sh
@@ -5,3 +5,6 @@ export CONVOX_RACK=$INPUT_RACK
 
 echo "Running rack params set on $INPUT_RACK for param $INPUT_PARAMNAME"
 convox rack params set $INPUT_PARAMNAME=$INPUT_PARAMVALUE
+
+echo "PARAM_VALUE=$output" >> $GITHUB_OUTPUT
+echo "PARAM_VALUE=$output" >> $GITHUB_ENV
