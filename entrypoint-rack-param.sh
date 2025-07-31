afd7d077d0287462b23d5765666f7338f838eb21
Tom Cummings <tom@smileidentity.com>
2025-07-31 09:03:12 -0400

    Revert "Output the rack param value"
    I'm an idiot
    
    This reverts commit 17caaa2d272cb5b0cbab3e3d6ec84721d75adb42.

--- entrypoint-rack-param.sh
+++ entrypoint-rack-param.sh
@@ -5,6 +5,3 @@ export CONVOX_RACK=$INPUT_RACK
 
 echo "Running rack params set on $INPUT_RACK for param $INPUT_PARAMNAME"
 convox rack params set $INPUT_PARAMNAME=$INPUT_PARAMVALUE
-
-echo "PARAM_VALUE=$output" >> $GITHUB_OUTPUT
-echo "PARAM_VALUE=$output" >> $GITHUB_ENV
